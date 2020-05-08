1f386e7f63b59f035fa915db9cb50f5a8a3cebbf
Bruno Lyra <53547531+lyra-solfieri@users.noreply.github.com>
2020-05-07 21:52:39 -0300

    fist test

---
+++ classes_pessoa_aluno_professor.py
@@ -0,0 +1,45 @@
+class Pessoa:
+    def __init__(self,nome,cpf,data):
+        super(Pessoa,self).__init__()
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nascimento = data
+
+
+
+    def __str__(self):
+
+        return str(self.nome,self.cpf,self.data_nascimento)
+
+
+
+class Aluno(Pessoa):
+
+   def __init__(self,nome,cpf,data,nota):
+       super(Aluno,self).__init__(nome,cpf,data)
+       self.nota = nota
+
+   def __float__(self,n):
+       return float(self.nota[n])
+
+   def __repr__(self):
+       return str(self.__dict__)
+   def __eq__(self, other):
+       return self.__dict__ == other.__dict__
+
+
+class Professor(Pessoa):
+
+    def __init__(self,nome,cpf,data,salario):
+        super(Professor,self).__init__(nome, cpf,data)
+        self.salario = salario
+
+    def __float__(self):
+        return float(self.salario)
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
---
+++ menu.py
@@ -0,0 +1,61 @@
+from classes_pessoa_aluno_professor import Aluno
+from classes_pessoa_aluno_professor import Professor
+
+nota = []
+aluno2 = []
+professor2 = []
+
+print("\n1.Cadastrar Aluno"
+      "\n2.Cadastrar Professor "
+      "\n3.Obter Média do aluno"
+      "\n4.Obter informações do Aluno"
+      "\n5.Obter informações do professor"
+      "\n0.Sair")
+flag = True
+while(flag):
+
+
+    x = int(input("Digite a opção: "))
+
+    if x == 1:
+
+        nome = input("Digite o nome do aluno: ")
+        cpf = input("Digite o cpf do aluno")
+        data = input("Data de nascimento do aluno (00/00/00) ")
+        for i in range(4):
+            n = int(input("Digite a {}º  nota: ".format(i+1)))
+            nota.append(n)
+
+        aluno = [Aluno(nome, cpf, data, nota)]
+        aluno2.append(aluno)
+
+    elif x == 2:
+        nome2 = input("Digite o nome do Professor: ")
+        cpf2 = input("Digite o cpf do Professor: ")
+        data2 = input("Data de nascimento do professor: ")
+        salario = float(input("Digite o salário do Professor: "))
+
+        professor = [Professor(nome2,cpf2,data2,salario)]
+        professor2.append(professor)
+
+    elif x == 3:
+        soma = 0
+
+        for numero in nota:
+            soma += numero
+        print("Nota final {}".format(soma/4))
+
+    elif x == 4:
+       print(aluno2)
+
+
+    elif x == 5:
+        print(professor2)
+
+    else:
+        print("Opção inválida, hasta la vista")
+        break
+
+
+
+
